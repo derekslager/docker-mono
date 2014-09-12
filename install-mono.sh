f61a411c3b84740531d8203f9f93a155fc295ca4
Derek Slager <derekslager@gmail.com>
2014-09-12 10:48:13 -0700

    Update for Mono 3.8.0.

--- install-mono.sh
+++ install-mono.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-curl -s http://download.mono-project.com/sources/mono/mono-3.6.0.tar.bz2 \
+curl -s http://download.mono-project.com/sources/mono/mono-3.8.0.tar.bz2 \
     | tar -xjv --directory=/tmp
 
-cd /tmp/mono-3.6.0
+cd /tmp/mono-3.8.0
 ./configure --prefix=/usr/local
 make
 make install
 cd /tmp
-rm -rf /tmp/mono-3.6.0
+rm -rf /tmp/mono-3.8.0
